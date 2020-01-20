296a8a17f8e67ca780c6155ce9016e5bf1b57d35
Martin Marosi <marvusm.mmi@gmail.com>
2020-01-20 17:31:11 +0100

    Added hooks base.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
 import ReduxShowCase from './components/redux-show-case';
+import LifeCycleTransform from './lifecycle-transform';
 
 function App() {
   return (
-    <div className="App">
-      <ReduxShowCase />
-    </div>
+    <ThemeProvider theme={createMuiTheme({})}>
+      <CssBaseline />
+      <Container maxWidth={false}>
+          <BrowserRouter>
+            <Switch>
+              <Route path="">
+                <LifeCycleTransform/>
+              </Route>
+              <Route path="/redux">
+                <ReduxShowCase />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+      </Container>
+    </ThemeProvider>
   );
 }
 
